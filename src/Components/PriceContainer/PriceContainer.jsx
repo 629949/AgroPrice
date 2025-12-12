4ad59b19884e8889692ab74d6b41a1ffa052366e
GadObua <gademma54@gmail.com.com>
2025-12-12 17:14:25 +0300

    add styles props to the div

--- src/Components/PriceContainer/PriceContainer.jsx
+++ src/Components/PriceContainer/PriceContainer.jsx
@@ -1,9 +1,15 @@
 import './Price.css'
 
-export default function PriceContainer({name, Price})
+export default function PriceContainer({name, Price, width, height})
 {
+
+    const Styles = {
+        height: height,
+        width: width
+    }
+
     return (
-        <div className='price-container'>
+        <div className='price-container' style={Styles}>
             <p>{name}</p>
             <p>{Price}</p>
         </div>
